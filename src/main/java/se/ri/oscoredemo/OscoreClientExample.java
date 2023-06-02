74fee7b1b32c9a8294cee9348d92e551b506a910
Rikard Höglund <rikard.hoglund@ri.se>
2023-06-02 16:41:27 +0200

    Extract OSCORE replay window size into own variable
    Signed-off-by: Rikard Höglund <rikard.hoglund@ri.se>

--- src/main/java/se/ri/oscoredemo/OscoreClientExample.java
+++ src/main/java/se/ri/oscoredemo/OscoreClientExample.java
@@ -51,13 +51,14 @@ public class OscoreClientExample {
 	private final static byte[] masterSecret = StringUtil.hex2ByteArray("0102030405060708090A0B0C0D0E0F10");
 	private final static byte[] masterSalt = StringUtil.hex2ByteArray("9e7ca92223786340");
 	private final static byte[] idContext = null;
+	private final static Integer replayWindowSize = 32;
 	private final static byte[] sid = new byte[] { 0x02 };
 	private final static byte[] rid = new byte[] { 0x01 };
 	private final static int MAX_UNFRAGMENTED_SIZE = 4096;
 
 	public static void main(String[] args) throws OSException, ConnectorException, IOException {
 		// Create and set OSCORE Security Context
-		OSCoreCtx ctx = new OSCoreCtx(masterSecret, true, alg, sid, rid, kdf, 32, masterSalt, idContext,
+		OSCoreCtx ctx = new OSCoreCtx(masterSecret, true, alg, sid, rid, kdf, replayWindowSize, masterSalt, idContext,
 				MAX_UNFRAGMENTED_SIZE);
 		db.addContext(uriBase, ctx);
 		OSCoreCoapStackFactory.useAsDefault(db);
--- src/main/java/se/ri/oscoredemo/OscoreServerExample.java
+++ src/main/java/se/ri/oscoredemo/OscoreServerExample.java
@@ -49,13 +49,14 @@ public class OscoreServerExample {
 	private final static byte[] masterSecret = StringUtil.hex2ByteArray("0102030405060708090A0B0C0D0E0F10");
 	private final static byte[] masterSalt = StringUtil.hex2ByteArray("9e7ca92223786340");
 	private final static byte[] idContext = null;
+	private final static Integer replayWindowSize = 32;
 	private final static byte[] sid = new byte[] { 0x01 };
 	private final static byte[] rid = new byte[] { 0x02 };
 	private final static int MAX_UNFRAGMENTED_SIZE = 4096;
 
 	public static void main(String[] args) throws OSException {
 		// Create and set OSCORE Security Context
-		OSCoreCtx ctx = new OSCoreCtx(masterSecret, false, alg, sid, rid, kdf, 32, masterSalt, idContext,
+		OSCoreCtx ctx = new OSCoreCtx(masterSecret, false, alg, sid, rid, kdf, replayWindowSize, masterSalt, idContext,
 				MAX_UNFRAGMENTED_SIZE);
 		db.addContext(ctx);
 		OSCoreCoapStackFactory.useAsDefault(db);
